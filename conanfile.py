74dd07a1832a8be891a4196e24d130020601b8cd
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:31 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,19 +3,19 @@ from conans import ConanFile
 
 class BoostCrcConan(ConanFile):
     name = "Boost.Crc"
-    version = "1.65.1"
+    version = "1.66.0"
     requires = \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Integer/1.65.1@bincrafters/testing"
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Integer/1.66.0@bincrafters/testing"
     lib_short_names = ["crc"]
     is_header_only = True
 
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-crc"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 
