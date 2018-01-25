16af56c9d280f4d201ab418a8be98d07f5a7e0d7
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:03 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostCrcConan(ConanFile):
     name = "boost_crc"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_crc"
-    
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["crc"]
     is_header_only = True
 
