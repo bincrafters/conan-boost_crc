0ea73f08647d40b3bedfea0f93def78cf8cd939b
Jerry W <jerrywiltse@gmail.com>
2019-01-26 23:58:37 -0500

    re-implement version numbers, fix travis badges, refresh ci scripts, move to python 3.6 on ci

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostCrcConan(base.BoostBaseConan):
     name = "boost_crc"
+    version = "1.68.0"
     url = "https://github.com/bincrafters/conan-boost_crc"
     lib_short_names = ["crc"]
     header_only_libs = ["crc"]
