47f34c7eca74647bd7e7982debcc0d571afec267
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:25 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,17 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostCrcConan(base.BoostBaseConan):
     name = "boost_crc"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_crc"
-    lib_short_names = ["crc"]
-    header_only_libs = ["crc"]
-    b2_requires = [
-        "boost_array",
-        "boost_config",
-        "boost_integer",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
